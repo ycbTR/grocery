eb4bf01557fa61ae0bff98222a81bc45b9402dbd
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2019-03-22 12:54:01 +0300

    push

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,7 +28,7 @@ class HomeController < ApplicationController
     # `ssh pi@192.168.1.83 "sudo ./kill_reader.sh"`
     # @response = r.gsub(/[^\d]/, '')
 
-    `sudo ./readRfid.sh`
+    system("sudo ./readRfid.sh)
   end
 
   def login
