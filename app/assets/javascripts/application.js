da4ca99d2e9103e737772922f936434a28b69ec4
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2019-03-23 17:48:48 +0300

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,113 @@
 //= require_tree ./channels
 //= require_tree .
 
-setInterval(function(){
+function get_loading_container() {
+    loadingContainer = $('.loader-big', this.el);
+}
+
+function hide_loading() {
+    get_loading_container();
+    loadingContainer.hide();
+}
+
+setInterval(function () {
     $('.alert').fadeOut();
-}, 3000) ;
+}, 3000);
+
+$(document).on("turbolinks:click", function () {
+    show_loading();
+    $(".loader").show();
+});
+
+$(document).on("turbolinks:load", function () {
+    loadingContainer.hide();
+});
+
+SHOW_LOADING = true;
+var loadingContainer;
+
+$(function () {
+    // Toggle loading container when ajax call
+    get_loading_container();
+    loadingContainer.ajaxStart(function (e) {
+        show_loading();
+    });
+
+    loadingContainer.ajaxStop(function (e) {
+        loadingContainer.hide();
+        $('#loaderMessage').html("");
+    });
+
+    $('form').on('submit', function () {
+        get_loading_container();
+        show_loading();
+    });
+});
+
+$("a[data-remote!='true'][data-fancybox-type!='iframe']").on('click', function (e) {
+    metaClick = e.metaKey || e.ctrlKey;
+    if (!metaClick && should_show_overlay($(this))) {
+        get_loading_container();
+        show_loading();
+    }
+});
+
+$("a[data-remote='true']").on('click', function (e) {
+        get_loading_container();
+        show_loading();
+});
+
+$(document).ready(function () {
+    get_loading_container();
+    loadingContainer.hide();
+    $('#loaderMessage').html("");
+});
+
+
+function should_show_overlay(el) {
+    return !el.attr("not-trigger-loading") &&
+        el.attr("href") &&
+        el.attr("href").slice(0, 1) != "#" &&
+        el.attr("href") != "javascript:;" &&
+        el.attr("href").slice(0, 1) != "" &&
+        el.attr("target") != "_blank" && !el.attr("data-confirm") &&
+        el.attr("data-method") != "delete"
+}
+
+function show_loading() {
+    if (!!SHOW_LOADING) {
+        if (typeof(reloadResourceInterval) != 'undefined')
+            clearInterval(reloadResourceInterval);
+        loadingContainer.css({
+            'opacity': '0',
+            'transition': 'transform 0.2s, opacity 0.2s',
+            'transform': 'scale(1.4)',
+            'display': 'block'
+        });
+        setTimeout(function () {
+            loadingContainer.css({
+                'opacity': '1',
+                'transform': 'scale(1)'
+            });
+        }, 50);
+    }
+}
+$(document).ready(function () {
+    $("[data-message]").on("click", function () {
+        message = $(this).attr('data-message');
+        $('#loaderMessage').html(message);
+    });
+});
+//
+//    window.ClientSideValidations.callbacks.form.pass = function (form, callback) {
+//        console.log('form-passs');
+//        get_loading_container();
+//        show_loading();
+//    }
+function redirect_to_target_and_show_loader(target) {
+    window.location.href = target;
+    setTimeout(function () {
+        show_loading();
+    }, 1);
+}
+
