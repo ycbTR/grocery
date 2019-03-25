a5c26c1250d8ed8c3f3fe8cf5e8bbbdc597025be
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2019-03-25 23:22:37 +0300

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_cable.url = "ws://127.0.0.1:3000/cable"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
