b00c62ba264924c2c12ab9f47789344e92915972
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2019-04-09 19:22:44 +0300

    adds caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,10 +5,10 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -49,6 +49,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.assets.js_compressor = :uglifier
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
