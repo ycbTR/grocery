5d423aae07cc2fd7cf84af2b57a52a1c78c7368b
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2019-04-18 20:28:45 +0300

    printer

--- lib/grocery/printer.rb
+++ lib/grocery/printer.rb
@@ -1,6 +1,13 @@
 module Printer
   extend self
   # https://learn.adafruit.com/networked-thermal-printer-using-cups-and-raspberry-pi?view=all
+
+  #** http://scruss.com/blog/2015/07/12/thermal-printer-driver-for-cups-linux-and-raspberry-pi-zj-58/
+
+  # https://github.com/klirichek/zj-58
+
+  # https://www.raspberrypi.org/forums/viewtopic.php?t=111669
+
   def print(object)
     val = if object.is_a? Order
             order_text(object)
